b19c66992ed1f87535517cb572dcb9ca8134f65a
Hutr0 <Hutro-games@yandex.ru>
2022-01-07 21:44:40 +0300

    MockTaskCell

--- ToDoAppTests/Controller/DataProviderTests.swift
+++ ToDoAppTests/Controller/DataProviderTests.swift
@@ -78,6 +78,29 @@ class DataProviderTests: XCTestCase {
         
         XCTAssertTrue(mockTableView.cellIsDequeued)
     }
+    
+    func testCellForRowInSectionZeroCallsConfigure() {
+        tableView.register(MockTaskCell.self, forCellReuseIdentifier: String(describing: TaskCell.self))
+        let task = Task(title: "Foo")
+        sut.taskManager?.add(task: task)
+        tableView.reloadData()
+        
+        let cell = tableView.cellForRow(at: IndexPath(row: 0, section: 0)) as! MockTaskCell
+        
+        XCTAssertEqual(cell.task, task)
+    }
+    
+//    func testCellForRowInSectionFirstCallsConfigure() {
+//        tableView.register(MockTaskCell.self, forCellReuseIdentifier: String(describing: TaskCell.self))
+//        let task = Task(title: "Foo")
+//        sut.taskManager?.add(task: task)
+//        sut.taskManager?.checkTask(at: 0)
+//        tableView.reloadData()
+//        
+//        let cell = tableView.cellForRow(at: IndexPath(row: 0, section: 1)) as! MockTaskCell
+//
+//        XCTAssertEqual(cell.task, task)
+//    }
 }
 
 extension DataProviderTests {
@@ -88,6 +111,14 @@ extension DataProviderTests {
             cellIsDequeued = true
             
             return super.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        }
+    }
+    
+    class MockTaskCell: TaskCell {
+        var task: Task?
+        
+        override func configure(withTask task: Task) {
+            self.task = task
         }
     }
 }
