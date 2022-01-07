cba2a10bb7f45ffab8679f7bbcacbe61c2efa94b
Hutr0 <Hutro-games@yandex.ru>
2022-01-07 19:10:47 +0300

    TaskListViewController part 2

---
+++ ToDoApp/Controller/DataProvider.swift
@@ -0,0 +1,26 @@
+//
+//  DataProvider.swift
+//  ToDoApp
+//
+//  Created by Леонид Лукашевич on 07.01.2022.
+//
+
+import UIKit
+
+class DataProvider: NSObject {
+    
+}
+
+extension DataProvider: UITableViewDelegate {
+    
+}
+
+extension DataProvider: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
--- ToDoApp/Controller/TaskListViewController.swift
+++ ToDoApp/Controller/TaskListViewController.swift
@@ -9,9 +9,9 @@ import UIKit
 
 class TaskListViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet var dataProvider: DataProvider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 }
-
