9a34dd7e9d2d5ee3a41604202d41a00f68e5debe
Hutr0 <Hutro-games@yandex.ru>
2022-01-08 17:38:40 +0300

    DetailViewController Elements

---
+++ ToDoApp/Controller/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.swift
+//  ToDoApp
+//
+//  Created by Леонид Лукашевич on 08.01.2022.
+//
+
+import UIKit
+import MapKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
+}
