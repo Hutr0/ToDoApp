bfb566e7d5589e2a99df747fbc48dd0a9adcbf5b
Hutr0 <Hutro-games@yandex.ru>
2022-01-10 02:01:16 +0300

    Model Serialization Tests

--- ToDoAppTests/Model/TaskTests.swift
+++ ToDoAppTests/Model/TaskTests.swift
@@ -53,5 +53,20 @@ class TaskTests: XCTestCase {
         
         XCTAssertEqual(location, task.location)
     }
+    
+    func testCanBeCreatedFromPlistDictionary() {
+        let location = Location(name: "Baz")
+        let date = Date(timeIntervalSince1970: 1604210400)
+        let task = Task(title: "Foo", description: "Bar", date: date, location: location)
+        
+        let locationDictionary: [String: Any] = ["name": "Baz"]
+        let dictionary: [String: Any] = ["title": "Foo",
+                                         "description": "Bar",
+                                         "date": date,
+                                         "location": locationDictionary]
+        let createdTask = Task(dict: dictionary)
+        
+        XCTAssertEqual(task, createdTask)
+    }
 }
  
